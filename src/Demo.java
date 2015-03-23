14b13be8898eae013a030f88aba071918a85454a
Michael Brown <mimbrown@mines.edu>
2015-03-23 15:02:47 -0600

    For everyone

--- src/Demo.java
+++ src/Demo.java
@@ -3,20 +3,38 @@ import java.util.*;
 
 public class Demo {
 	
-	public static String findPal(String s) {
-		if(isPalindrome(s)) {
+	public static String findPalRecurs(String s) {
+		/*if(isPalindrome(s)) {
 			return s;
 		}
 		String copy, longestPal = "";
 		List<String> possibles = new ArrayList<String>(s.length());
 		for(int i = 0; i < s.length(); i++) {
 			copy = remove(s, i);
-			possibles.add(i, findPal(copy));
+			possibles.add(i, findPalRecurs(copy));
 		}
 		for(String c : possibles) {
 			if(c.length() > longestPal.length()) longestPal = c;
 		}
+		return longestPal;*/
+		if(s.length() == 0 || s.length() == 1) return s;
+		int front = 0, back = s.length()-1;
+		List<String> possibles = new ArrayList<String>(s.length());
+		if(s.charAt(front) == s.charAt(back)) {
+			return Character.toString(s.charAt(front)) +
+					findPalRecurs(s.substring(front + 1, back - 1) + Character.toString(s.charAt(back)));
+		}
+		possibles.add(findPalRecurs(s.substring(front + 1, back)));
+		possibles.add(findPalRecurs(s.substring(front, back - 1)));
+		String longestPal = "";
+		for(String c : possibles) {
+			if(c.length() > longestPal.length()) longestPal = c;
+		}
 		return longestPal;
+	}
+	
+	public static String findPalDynam(String s) {
+		return null;
 	}
 	
 	public static String remove(String s, int i) {
@@ -44,9 +62,11 @@ public class Demo {
 		System.out.print("Enter a string: ");
 		String s = reader.nextLine();
 		long t1 = System.currentTimeMillis();
-		System.out.println(findPal(s));
+		System.out.println(findPalRecurs(s));
+		//System.out.println(findPalDynam(s));
 		long t2 = System.currentTimeMillis();
 		System.out.println((t2 - t1)/1000.0);
+		reader.close();
 	}
 
 }
